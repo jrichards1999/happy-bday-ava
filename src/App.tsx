2127d7a4239523074e9ed48ffa42bb75024a4a65
jrichards1999 <jrichards050399@gmail.com>
2023-11-16 18:04:56 -0500

    Added extra text and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
    return (
       <div className="App">
          <div className="App-header">
-            <h1>HAPPY BIRTHDAY AVA</h1>
+            <h1 className="spinning-text styled-text">HAPPY BIRTHDAY AVA!!</h1>
+            <h1 className="styled-text">Happy birthday my love!!</h1>
+            <h1 className="styled-text">
+               I was gonna do more with this but I kinda ran outta time lol
+            </h1>
+            <h1 className="styled-text">
+               But I hope you have a wonderful birthday honey!!
+            </h1>
+            <h1 className="styled-text">I love you so so much :)</h1>
+            <h1 className="styled-text">ope</h1>
          </div>
       </div>
    );
